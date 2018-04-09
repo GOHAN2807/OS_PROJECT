2980a6562c680c8f8a241b5e90c3d7c4dcafbe29
GOHAN2807 <36929808+GOHAN2807@users.noreply.github.com>
2018-04-09 20:40:11 +0530

    Update PID_manager.c

--- PID_manager.c
+++ PID_manager.c
@@ -29,6 +29,36 @@ int allocate_map(void)
 	else
 	return -1;
 }
+int allocate_pid(void)
+{
+	int n_loops = 0;
+
+	int next;
+
+	if (last == PID_MAX)
+		next = PID_MIN-100;
+	else
+		next = last + 1-100;
+
+	while ( (n_loops <= (PID_MAX - PID_MIN)) && (pArr[next].bitmap == 1) ) {	
+		++n_loops;
+		++next;
+		if (next > PID_MAX)
+			next = PID_MIN;
+	}
+
+	if (n_loops == (PID_MAX - PID_MIN) + 1) {
+		return -1;
+	}
+	
+	pArr[next].bitmap = 1;
+	pArr[next].pid = next + 100;
+
+	last = next+100;
+
+	return last;
+}
+
 void release_pid(int pid)
 {
 	int i;
